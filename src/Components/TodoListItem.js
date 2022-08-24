a2de3d09e3ad66f108eb6d714654a03f19c8aea5
Eman <emanwork369@gmail.com>
2022-08-24 13:22:49 +0200

    Added All and Uncompleted Buttons

--- src/Components/TodoListItem.js
+++ src/Components/TodoListItem.js
@@ -8,26 +8,29 @@ const TodoListItem = ({ details, deleteItem, changeTitle, togglecompletedItem}) 
     const [didCompleted, setDidCompleted] = useState(false);
     const [editMode, setEditMode] = useState(false);
 
+    // Completed Todo Item Handler
     const completedItemHandler = (id) => {
          setDidCompleted(!didCompleted);
          togglecompletedItem(id);
     }
 
+    // Toggle Edit Mode
     const toggleMode = () =>{
         setEditMode(!editMode);
     }
 
     return (
-        <li className="todo-item-container">
+        // Todo Item
+        <li className="todo-item-container" style={ {display : details.display ? 'block' : 'none'} } >
             <header>
                 <div className='header-text-container'>
-                    <span>{didCompleted || details.completed  ? '✅' : <ItemPriority priority={ details.priority }/> }</span>
+                    <span>{ details.completed ? '✅' : <ItemPriority priority={ details.priority }/> }</span>
                     {
                         editMode ? 
                         <form>
                             <input type="text" value={details.text}  onChange={ e => changeTitle(e ,details.id, e.target.value)}/>
                         </form> :
-                        <h3 className={didCompleted || details.completed ? 'item-completed' : '' } onClick={() => completedItemHandler (details.id)}>{details.text}</h3>
+                        <h3 className={details.completed ? 'item-completed' : '' } onClick={() => completedItemHandler (details.id)}>{details.text}</h3>
                     }
                 </div>
                 <button className='btn remove-btn' onClick={() => deleteItem(details.id)}>   
--- src/Pages/Todo.js
+++ src/Pages/Todo.js
@@ -5,29 +5,34 @@ import '../styles/todo-page.css';
 
 const Todo = () => {
 
+    // Todo default List
     const [todoListItems, setTodoList] = useState(
         [
             {
               id: 1,
               text: 'Learn React',
               completed: false,
-              priority: 'LOW'
+              priority: 'LOW',
+              display: true
             },
             {
               id: 2,
               text: 'Learn JS',
               completed: false,
-              priority: 'URGENT'
+              priority: 'URGENT',
+              display: true
             },
             {
               id: 3,
               text: 'Learn Anything',
               completed: true,
-              priority: 'MEDIUM'
+              priority: 'MEDIUM',
+              display: true
             },
         ]
     );
 
+    // Add Todo Handler
     const addItemHandler = (text, priority) => {
       setTodoList([...todoListItems, {
         id : new Date().toString(),
@@ -38,10 +43,12 @@ const Todo = () => {
       }]);
     }
 
+    // Delete Todo Items Handler
     const deleteItemHandler = (id) => {
       setTodoList(todoListItems.filter(item => item.id !== id));
     }
 
+    // Toggle Completed Todo Items Handler
     const togglecompletedItemHandler = (id) => {
       setTodoList(todoListItems.map( item => {
         if (item.id === id) {
@@ -52,13 +59,45 @@ const Todo = () => {
       ))
     }
 
+    // Change Todo Items Title Handler
     const changeTodoTitleHandler  = (e, id, newTitle) => {
         e.preventDefault();
         setTodoList(todoListItems.filter(item => item.id === id ? item.text = newTitle : item ))
     }
 
-    const fitlterCompletedTodoHandler = () => {
-      setTodoList(todoListItems.filter(item => item.completed ? item : ""))
+    // Display All Todo Items Handler
+    const displayAllTodoHandler = () => {
+      setTodoList(todoListItems.map(item => {
+          item.display = true;
+          return item;
+        }
+      ))
+    }
+
+    // Display All Completed Todo Items Handler
+    const displayCompletedTodoHandler = () => {
+      setTodoList(todoListItems.map(item => {
+        if (!item.completed) {
+          item.display = false
+        } else {
+          item.display = true
+        }
+        return item;
+        })
+      )
+    }
+
+    // Dispaly All Completed Todo Items Handler
+    const displayUnCompletedTodoHandler = () => {
+      setTodoList(todoListItems.map(item => {
+        if (item.completed) {
+          item.display = false
+        } else {
+          item.display = true
+        }
+        return item;
+        })
+      )
     }
 
     return (
@@ -66,10 +105,14 @@ const Todo = () => {
           <header className='page-header-container'>
             <h1>Todo List 📌</h1>
           </header>
-          <button className="completed-todo-btn" onClick={() => fitlterCompletedTodoHandler ()}>Display All Completed Todo</button>
           <AddTodoForm addItem = { addItemHandler }/>
           <div className="todo-lists-container">
             <TodoList items = { todoListItems} deleteItem={ deleteItemHandler } changeTitle={changeTodoTitleHandler} togglecompletedItem={togglecompletedItemHandler}/>
+          </div>
+          <div className="btns-control-todolist">
+            <button className="completed-todo-btn" onClick={() => displayAllTodoHandler ()}>All</button>
+            <button className="completed-todo-btn" onClick={() => displayCompletedTodoHandler ()}>Completed</button>
+            <button className="completed-todo-btn" onClick={() => displayUnCompletedTodoHandler ()}>Un Completed</button>
           </div>
         </div>
     )
